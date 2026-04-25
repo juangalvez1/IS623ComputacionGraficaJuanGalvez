437780687b36f70851ba1b4872e07be56b72b40d
ann123-max <ana.cabrales@utp.edu.co>
2026-04-24 21:33:44 -0500

    Second figure of task 1 of parcial 2 completed

--- Parcial2/Punto1/dibujitos.js
+++ Parcial2/Punto1/dibujitos.js
@@ -83,3 +83,68 @@ dibujarFiguraRellena(isometrico1, [
 ], "#C84AFA");
 
 // Figurita 2
+
+// Cara frontal izquierda
+dibujarFiguraRellena(isometrico2, [
+    [100, 420],
+    [0, 360],
+    [0, 240],
+    [100, 300]
+], "#39DAFA");
+
+// Primer escalon plano
+dibujarFiguraRellena(isometrico2, [
+    [0, 240],
+    [75, 195],
+    [175, 255],
+    [100, 300]
+], "#39DAFA");
+
+// Primer escalon vertical
+dibujarFiguraRellena(isometrico2, [
+    [175, 255],
+    [75, 195],
+    [75, 135],
+    [175, 195]
+], "#39DAFA");
+
+// Segundo escalon plano
+dibujarFiguraRellena(isometrico2, [
+    [175, 195],
+    [75, 135],
+    [134, 100],
+    [234, 160]
+], "#39DAFA");
+
+//Segundo escalon vertical
+dibujarFiguraRellena(isometrico2, [
+    [234, 160],
+    [134, 100],
+    [134, 40],
+    [234, 100]
+], "#39DAFA");
+
+//Tercer escalon plano 
+dibujarFiguraRellena(isometrico2, [
+    [234, 100],
+    [134, 40],
+    [200, 0],
+    [300, 60]
+], "#39DAFA");
+
+
+//Cara lateral izquierda
+dibujarFiguraRellena(isometrico2, [
+    [100, 300],
+    [175, 255],
+    [175, 195],
+    [234, 160],
+    [234, 100],
+    [300, 60],
+    [300, 300],
+    [100, 420]
+], "#39DAFA");
+
+
+
+
